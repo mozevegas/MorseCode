024debe29957c4b026101105e37062fc9687e909
moevegas <moevegas@gmail.com>
2017-03-30 00:02:34 -0400

    Morse Code translates

--- MorseCode/Program.cs
+++ MorseCode/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,61 @@ namespace MorseCode
     {
         static void Main(string[] args)
         {
+
+            var morseFile = "morse.csv";
+
+            Dictionary<string, string> morseDiction = new Dictionary<string, string>();
+            morseDiction = new Dictionary<string, string>();
+            using (var writing = new StreamReader(morseFile))
+            {
+                while (writing.Peek() > -1)
+                {
+                    var line = writing.ReadLine().Split(',');// ["B","..--"]
+                    morseDiction.Add(line[0], line[1]);
+                }
+            }
+
+            // Testing for proof
+            foreach (KeyValuePair<string, string> element in morseDiction)
+            {
+                Console.WriteLine(element.Key + element.Value);
+            }
+
+            Console.WriteLine("Input a word you would like to convert to Morse Code");
+            string conVerto = Console.ReadLine();
+            conVerto = conVerto.ToUpper();
+
+
+            //string upConvo = conVerto.ToUpper();
+            Console.WriteLine($"You would like to convert {conVerto.ToUpper()}");
+            Console.WriteLine(conVerto);
+
+
+            // convert string to characters and return translation from Morse
+
+
+            Console.WriteLine($"here is F {morseDiction["F"]}");
+
+            if (morseDiction.ContainsKey("A"))
+            {
+                Console.WriteLine(morseDiction.Values);
+            }
+
+
+            // convert to Character Array
+            char[] conAir = conVerto.ToCharArray();
+
+            // conAir is an array--Proof
+            foreach (var here in conAir)
+            {
+                Console.Write(here);
+                var herepart = here.ToString();
+                Console.WriteLine($" ==  {morseDiction[herepart]}");
+            }
+
+
+
+
         }
     }
 }
